735fe442ad86d5721a4a2f630f397b235867419c
anrosent <Anson_Rosenthal@brown.edu>
2014-10-12 21:13:33 -0400

    PHPy HTTP server works, piggybacking off http.server.SimpleHTTPServer. some tests as well, tho the semantics of consecutive code blocks, some of which don't produce any output, could be revised to kill the resulting whitespace. This would require more complex templating, since the content outside the code block needs to be modified conditional on the code in the block

--- capture.py
+++ capture.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-#Python Hypertext Preprocessor: PHPy
-#Author: Anson Rosenthal (anrosent)
-#10/2013
+#!/usr/bin/env python3
 
-#3rd and final iteration. Regex sub >> parsing it yourself when structure is so simple!
-import re, sys, ast
+import re, sys, ast, fileinput
 
 #Delimiters of code elements
 code_start = "<?"
@@ -15,30 +11,40 @@ find_code = re.compile("%s(.*?)%s"%(re.escape(code_start), re.escape(code_stop))
 
 #Class to execute code and capture output from print statements - the idea is to mirror PHP's echo
 class CaptureBuffer():
+
+    # Init with dict to save state between code blocks and self to capture printed output
     def __init__(self):
         self.string = ''
         self.capture = ''
         self.var_dict = {'output_buffer':self}
         
+    # Adds new code block into buffer
     def add(self, string):
         self.string = string
         self.capture = ''
         
-    #Implement file writing interface so calls to 'print' can be redirected to buffer
+    # Implement file writing interface so calls to 'print' can be redirected to buffer
     def write(self, string):
-        self.capture += string
+        if self.capture:
+            self.capture += string
+        else:
+            self.capture += string
         
+    # Sets the params we don't really care about of an AST node we inject
     def fake_node_pos(self, node):
         node.lineno = 0
         node.col_offset = 0
         return node
         
+    # Return AST node containing name ref to this buffer
     def self_node(self):
         return self.fake_node_pos(ast.Name('output_buffer', ast.Load()))
     
+    # Return empty string AST node
     def str_node(self):
         return self.fake_node_pos(ast.Str(''))
     
+    # Returns keyword args we inject into print calls
     def get_print_keywords(self):
         return [ast.keyword('file',self.self_node()), ast.keyword('end', self.str_node())]
     
@@ -46,36 +52,54 @@ class CaptureBuffer():
     #captured output
     def capture_output(self):
         tree = ast.parse(self.string)
+
         for node in ast.walk(tree):
             if isinstance(node, ast.Call):
+
+                # If this node is a call to print(), set the output file to this buffer
                 if node.func.id == 'print':
                     node.keywords.extend(self.get_print_keywords())
+
+        # Execute code block with same local state as other code blocks
         exec(compile(tree, '<string>', 'exec'), self.var_dict)
+
+        # Reset buffer
         self.string = ''
-        return self.capture
+
+        # Return captured output
+        return self.capture.strip() + '' if self.capture else ''
     
-capture_buf = CaptureBuffer()    
     
 #For each match, replace it with its output    
 def process(string):
-    return find_code.sub(capture_output, string)
+    
+    # Init a buffer
+    capture_buf = CaptureBuffer()    
+
+    # Find and replace all code blocks with their output
+    #
+    # Note that this replacment is done in sequence by sub(), so we can rely on sequential
+    # execution of our code blocks
+    return find_code.sub(output_capturer(capture_buf), string)
 
 #Captures output of each matched code element to be substituted for matched text    
-def capture_output(match):
-    contents = match.groups()[0].strip()
-    capture_buf.add(contents)
-    return capture_buf.capture_output()
-    
+def output_capturer(buf):
+
+    def capture(match):
+        # Get code block text
+        contents = match.groups()[0].strip()
+
+        # Add code to buffer
+        buf.add(contents)
+
+        # Execute and return captured output
+        return buf.capture_output()
+
+    # Return closure over buffer
+    return capture
+
 #Runs transformation on input file and prints result to stdout
 #Usage: python phpy.py [input file]
 #   if no input file specified, reads from stdin and prints result to stdout    
-#   FEATURE NOTE: single-pass parse and evaluate INSTEAD OF full read and store, then eval and replace
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print(process(''.join(sys.stdin.readlines())))
-    elif len(sys.argv) == 2:
-        with open(sys.argv[1]) as inputfile:
-            print(process(inputfile.read()))
-    else:
-        print("Usage: python phpy.py [input_file]")
-    
+    print(process(''.join(fileinput.input())))
